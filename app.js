07c4d5f4386a65293ad96b4d6aa006bf40179cfb
Jorge Gonzalez Peregrin <jorgevik@gmail.com>
2016-10-18 18:21:05 +0200

    first commit

---
+++ app.js
@@ -0,0 +1,14 @@
+/**
+ * Created by Georgevik on 18/10/2016.
+ */
+
+'use strict';
+
+var express = require('express');
+var app = express();
+
+app.get('/',function(req,res) {
+	res.send("Hello World");
+});
+
+app.listen(4000);
